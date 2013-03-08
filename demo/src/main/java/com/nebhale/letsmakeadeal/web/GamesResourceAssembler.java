51147c9d64e63ea1a1a43a4aaef2f25655070922
nazaret.kazarian@gmail.com <nazaret.kazarian@gmail.com>
2013-03-08 18:44:50 +0200

    moved to demo dir and added presentation

---
+++ demo/src/main/java/com/nebhale/letsmakeadeal/web/GamesResourceAssembler.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nebhale.letsmakeadeal.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.stereotype.Component;
+
+import com.nebhale.letsmakeadeal.Game;
+
+@Component
+final class GamesResourceAssembler {
+
+    private final GameResourceAssembler gameResourceAssembler;
+
+    @Autowired
+    GamesResourceAssembler(GameResourceAssembler gameResourceAssembler) {
+        this.gameResourceAssembler = gameResourceAssembler;
+    }
+
+    GamesResource toResource(List<Game> games) {
+        
+    	GamesResource resource = new GamesResource();
+
+        for (Game game : games) {
+            resource.games.add(this.gameResourceAssembler.toResource(game));
+        }
+        
+        // self = /games
+        Link selfLink = ControllerLinkBuilder.linkTo(GamesController.class).withSelfRel();
+		resource.add(selfLink);
+
+        return resource;
+    }
+
+}
